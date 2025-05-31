14ffb72c184f1ed081a7666cdb905a100f4ab88e
Wahyuni Fajrin Rosyidah <89108001+wahyunirosyidah@users.noreply.github.com>
2025-05-31 07:22:40 +0700

    uodate

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 import streamlit as st
 import numpy as np
-from PIL import Image
-from tensorflow.keras.models import load_model
+# from PIL import Image
+# from tensorflow.keras.models import load_model
 st.markdown("💡 Bergabung sebagai Duta Noken adalah langkah kecil untuk dampak budaya yang besar.")
