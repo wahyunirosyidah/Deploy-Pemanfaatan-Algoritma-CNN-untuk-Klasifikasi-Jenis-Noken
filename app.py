6573b6bb410addbf7cfaf2e6aa8906fcf8276e63
Wahyuni Fajrin Rosyidah <89108001+wahyunirosyidah@users.noreply.github.com>
2025-05-31 10:47:31 +0700

    updaate

--- app.py
+++ app.py
@@ -1,6 +1,14 @@
 import streamlit as st
 import numpy as np
 from PIL import Image
-import tensorflow
+import tensorflow as tf
 # from tensorflow.keras.models import load_model
 st.markdown("💡 Bergabung sebagai Duta Noken adalah langkah kecil untuk dampak budaya yang besar.")
+
+
+@st.cache(allow_output_mutation=True)
+def load_model():
+  model=tf.keras.models.load_model('mobilenetv2.h5')
+  return model
+with st.spinner('Model is being loaded..'):
+  model=load_model()
